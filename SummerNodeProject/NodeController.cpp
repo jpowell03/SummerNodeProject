d44a470b696bd68871eb4a587f20491e0cf1668e
Henrichsen <cody.henrichsen@740s-pl-127.local>
2016-07-26 15:31:07 -0600

    Added new class NodeController and two functions tryNode and start

---
+++ SummerNodeProject/NodeController.cpp
@@ -0,0 +1,23 @@
+//
+//  NodeController.cpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "NodeController.h"
+
+
+void NodeController :: tryNodes()
+{
+    
+}
+
+void NodeController :: start()
+{
+    tryNodes();
+}
+
+
+
---
+++ SummerNodeProject/NodeController.h
@@ -0,0 +1,22 @@
+//
+//  NodeController.h
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef NodeController_h
+#define NodeController_h
+
+#include <stdio.h>
+
+class NodeController
+{
+private:
+    void tryNodes();
+    
+public:
+    void start();
+};
+#endif /* NodeController_h */
