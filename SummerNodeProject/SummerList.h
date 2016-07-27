5fd669da90440a8f077af70e8aeb0b9c1a5c37b6
jpowell03 <cody.henrichsen@740s-pl-127.local>
2016-07-27 13:41:30 -0600

    Added SummerList class

---
+++ SummerNodeProject/SummerList.h
@@ -0,0 +1,38 @@
+//
+//  SummerList.h
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/27/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef SummerList_h
+#define SummerList_h
+
+#include <stdio.h>
+#include "DataNode.h"
+
+#endif /* SummerList_h */
+
+template<class Type>
+class SummerList
+{
+private:
+    int size;
+    DataNode<Type> * front;
+    DataNode<Type> * end;
+    
+public:
+    Type getFromIndex(int index);
+    Type getFirst();
+    Type getLast();
+    void sort();
+    void setAtIndex(int index, Type data);
+    int getSize();
+    void addAtIndex(int index, Type data);
+    void add(Type data);
+    void addAtFront(Type data);
+    Type remove(int index);
+    bool contains(Type data);
+    
+};
